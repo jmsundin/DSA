aa017a3e16367e04ddacb0e6e371f34c70db3454
Jon Sundin <sundinjon@gmail.com>
2023-11-02 23:54:13 -0700

    added two pointer technique solution

--- algorithms/JavaScript/src/strings/palindrome.js
+++ algorithms/JavaScript/src/strings/palindrome.js
@@ -15,8 +15,25 @@ function isPalindrome(str) {
   return properStr === reverseStr;
 }
 
+function isPalindromeWithTwoPointers(str) {
+  let left = 0;
+  left right = str.length - 1;
+
+  while (left < right) {
+    if (str[left] !== str[right]) return false;
+    left++;
+    right--;
+  }
+  return true;
+}
+
 // Output to the console
 console.log(isPalindrome('eye'));
 console.log(isPalindrome('Mr. Owl ate my metal worm'));
 console.log(isPalindrome('RAce C*_aR'));
 console.log(isPalindrome('asdfggfrd'));
+
+console.log(isPalindromeWithTwoPointers('eye'));
+console.log(isPalindromeWithTwoPointers('Mr. Owl ate my metal worm'));
+console.log(isPalindromeWithTwoPointers('RAce C*_aR'));
+console.log(isPalindromeWithTwoPointers('asdfggfrd'));
